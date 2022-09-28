ad0f3408179db949be020fd65633169759ce1b38
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-09-28 11:33:29 -0700

    Initial commit

---
+++ R/env-get.R
@@ -0,0 +1,48 @@
+#' Working with the modelenv model environment
+#'
+#' These functions read and write to the environment where the package stores
+#' information about model specifications.
+#'
+#' @param items A character string of objects in the model environment.
+#' @param ... Named values that will be assigned to the model environment.
+#' @param name A single character value for a new symbol in the model
+#'   environment.
+#' @param value A single value for a new value in the model environment.
+#' @keywords internal
+#' @examples
+#' # Access the model data:
+#' current_code <- get_model_env()
+#' ls(envir = current_code)
+#'
+#' get_from_env("models")
+#' get_from_env("modes")
+#'
+#' get_from_env("example")
+#' set_env_val("example", 4)
+#' get_from_env("example")
+#' @keywords internal
+#' @export
+get_model_env <- function() {
+  utils::getFromNamespace("modelenv", ns = "modelenv")
+}
+
+#' @rdname get_model_env
+#' @keywords internal
+#' @export
+get_from_env <- function(items) {
+  mod_env <- get_model_env()
+  rlang::env_get(mod_env, items, default = NULL)
+}
+
+#' @rdname get_model_env
+#' @keywords internal
+#' @export
+set_env_val <- function(name, value) {
+  if (length(name) != 1 || !is.character(name)) {
+    rlang::abort("`name` should be a single character value.")
+  }
+  mod_env <- get_model_env()
+  x <- list(value)
+  names(x) <- name
+  rlang::env_bind(mod_env, !!!x)
+}
---
+++ R/env.R
@@ -0,0 +1,36 @@
+## Rules about model-related information
+
+### Definitions:
+
+# - the model is the model type (e.g. "k_means", etc)
+# - the model's mode is the species of model such as "partition"
+# - the engines are within a model and mode and describe the
+#   method/implementation of the model in question. These are often R package
+#   names.
+
+### The package dependencies are model- and engine-specific.
+### They are used across modes
+
+### The `fit` information is a list of data that is needed to fit the model.
+### This information is specific to an engine and mode.
+
+### The `predict` information is also list of data that is needed to make some
+### sort of prediction on the model object. The possible types are contained in
+### `pred_types` and this information is specific to the engine, mode, and type
+### (although there are no types across different modes).
+
+# ------------------------------------------------------------------------------
+
+# Initialize model environments
+
+all_modes <- c("partition")
+
+# ------------------------------------------------------------------------------
+
+pred_types <- c("cluster", "raw")
+
+# ------------------------------------------------------------------------------
+
+modelenv <- rlang::new_environment()
+modelenv$models <- NULL
+modelenv$modes <- c(all_modes, "unknown")
