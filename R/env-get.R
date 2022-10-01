4a46cd689c023966d816a1c8011d3c2481c5a2b3
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-09-30 18:35:09 -0700

    remove all keyword internal

--- R/env-get.R
+++ R/env-get.R
@@ -4,11 +4,9 @@
 #' information about model specifications.
 #'
 #' @param items A character string of objects in the model environment.
-#' @param ... Named values that will be assigned to the model environment.
 #' @param name A single character value for a new symbol in the model
 #'   environment.
 #' @param value A single value for a new value in the model environment.
-#' @keywords internal
 #' @examplesIf FALSE
 #' # Access the model data:
 #' current_code <- get_model_env()
@@ -20,14 +18,12 @@
 #' get_from_env("example")
 #' set_env_val("example", 4)
 #' get_from_env("example")
-#' @keywords internal
 #' @export
 get_model_env <- function() {
   utils::getFromNamespace("modelenv", ns = "modelenv")
 }
 
 #' @rdname get_model_env
-#' @keywords internal
 #' @export
 get_from_env <- function(items) {
   mod_env <- get_model_env()
@@ -35,7 +31,6 @@ get_from_env <- function(items) {
 }
 
 #' @rdname get_model_env
-#' @keywords internal
 #' @export
 set_env_val <- function(name, value) {
   if (length(name) != 1 || !is.character(name)) {
