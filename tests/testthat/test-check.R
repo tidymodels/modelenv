a2f454245d3112fe2b19f08f44f40563125f4165
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-10-14 14:15:04 -0700

    add tests for stop_incompatible_mode

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -8,3 +8,95 @@ test_that("check_spec_mode_engine_val() works", {
     check_spec_mode_engine_val("hoop", "raw", "stats")
   )
 })
+
+test_that("stop_incompatible_mode() works", {
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(spec_modes = "partition")
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(spec_modes = c("partition", "raw"))
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(spec_modes = c("partition", "raw", "another one"))
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = "partition",
+      eng = "stats"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw"),
+      eng = "stats"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw", "another one"),
+      eng = "stats"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = "partition",
+      model = "shallow"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw"),
+      model = "shallow"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw", "another one"),
+      model = "shallow"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = "partition",
+      eng = "stats",
+      model = "shallow"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw"),
+      eng = "stats",
+      model = "shallow"
+    )
+  )
+
+  expect_snapshot(
+    error = TRUE,
+    stop_incompatible_mode(
+      spec_modes = c("partition", "raw", "another one"),
+      eng = "stats",
+      model = "shallow"
+    )
+  )
+})
